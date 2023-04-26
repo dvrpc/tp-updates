319ea6eb7a989521e07df7283e47f5f0f0d213eb
Kris Warner <kdwarn@protonmail.com>
2023-04-26 13:32:13 -0400

    Add staging on https for origin

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ app.add_middleware(
         "http://linux3.dvrpc.org",
         "http://intranet.dvrpc.org",
         "http://staging.dvrpc.org",
+        "https://staging.dvrpc.org",
         "https://dvrpc.org",
         "https://www.dvrpc.org",
     ],
